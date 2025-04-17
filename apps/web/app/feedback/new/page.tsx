ad7b7bd0eac75ce6583f34745e255635a51ad276
KangAbbad <naufalabbad9@gmail.com>
2025-04-17 14:42:34 +0700

    refactor(feedback): update query key and service imports for consistency
    Move query key and service imports from local to shared directory to maintain consistency across feedback feature modules

--- apps/web/app/feedback/new/page.tsx
+++ apps/web/app/feedback/new/page.tsx
@@ -5,14 +5,14 @@ import {
 } from "@tanstack/react-query";
 
 import FeedbackFormPage from "./page.client";
-import { queryKey } from "./libs/constants";
-import { getCategoryList } from "./services/get";
+import { queryKey as queryKeyFeedbackList } from "../(list)/libs/constants";
+import { getCategoryList } from "../(list)/services/get";
 
 export default async function Page() {
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
-    queryKey: [queryKey.FEEDBACK_CATEGORY_LIST],
+    queryKey: [queryKeyFeedbackList.FEEDBACK_CATEGORY_LIST],
     queryFn: getCategoryList,
   });
 
